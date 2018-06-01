b3960486327e17399bb1dcd40f2b04e7d3413c19
Valérian <valerian.de-thezan-de-gaussan@etu.univ-lyon1.fr>
2018-06-01 17:09:45 +0200

    Strategie prudente OK + clean

--- Configuration.py
+++ Configuration.py
@@ -1,4 +1,5 @@
 import pulp
+import pickle
 
 
 class Configuration:
@@ -29,38 +30,24 @@ class Configuration:
         elif y == x and t == 0:
             self.g_opt = 0
 
-    def get_min_g(self, table_locale):
-        minimum = table_locale[1, 1]
-        for cle in table_locale:
-                minimum = min(minimum, table_locale[cle])
-        return minimum
-
-    def get_max_g(self, table_locale):
-        maximum = table_locale[1, 1]
-        for cle in table_locale:
-                maximum = max(maximum, table_locale[cle])
-        return maximum
-
     def calc_gs_opt(self, table_conf):
-        cond = self.x == 3 and self.y == 3 and self.t == 0
-
         # Tout d'abord, on crée notre table 'locale' de g_opt correspondant à tous les prochains coups possibles
         table_locale = dict()
-        for pierre_jete_par_x in range(1, self.x + 1):
-            for pierre_jete_par_y in range(1, self.y + 1):
+        for i in range(1, self.x + 1):
+            for j in range(1, self.y + 1):
 
-                if pierre_jete_par_x < pierre_jete_par_y:
+                if i < j:
                     troll = self.t - 1
                     if troll < -self.mp:  # Au cas où le troll va a + ou - (m' + 1)
                         troll = -self.mp
-                elif pierre_jete_par_x > pierre_jete_par_y:
+                elif i > j:
                     troll = self.t + 1
                     if troll > self.mp:
                         troll = self.mp
                 else:
                     troll = self.t
 
-                table_locale[pierre_jete_par_x, pierre_jete_par_y] = table_conf[self.x - pierre_jete_par_x, self.y - pierre_jete_par_y, troll].g_opt
+                table_locale[i, j] = table_conf[self.x - i, self.y - j, troll].g_opt
 
         # On crée un problème linéaire de maximisation avec PuLP
         plne = pulp.LpProblem("calc", pulp.LpMaximize)
@@ -71,9 +58,7 @@ class Configuration:
         for i in range(1, self.x + 1):
             plne_vars[i] = pulp.LpVariable("p"+str(i), lowBound=0, upBound=1)
 
-        g_min = self.get_min_g(table_locale)
-        g_max = self.get_max_g(table_locale)
-        g_opt = pulp.LpVariable("gain_opt", lowBound=g_min, upBound=g_max)
+        g_opt = pulp.LpVariable("gain_opt", lowBound=-1, upBound=1)
 
         # On rajoute au problème notre objectif
         plne += g_opt
@@ -100,42 +85,43 @@ class Configuration:
         self.s_opt = dict()
         for k in plne_vars:
             self.s_opt[k] = plne_vars[k].varValue
-        print(self.__str__())
+        # print(self.__str__())
 
     def __str__(self):
         return "Configuration: x:{}, y:{}, t:{}, g_opt:{}, s_opt:{}".format(self.x, self.y, self.t, self.g_opt, self.s_opt)
 
 
-def debug(cond, message):
-    if cond:
-        print(message)
+def export_fichier(n, m, dict_a_sauv):
+    # On transforme le dict de configuration en un dict simple de s_opt.
+    dict_s_opt = dict()
+    for key in dict_a_sauv:
+        dict_s_opt[key] = dict_a_sauv[key].s_opt
+
+    with open('data/{}-{}-{}.json'.format(n, n, m), 'wb') as conf_file:
+        pickle.dump(dict_s_opt, conf_file)
 
 
 def main():
-    N = 15
-    M = 7
-    Mp = M // 2
-    table = dict()
-    # Les cas triviaux sont initialisés lors de la creation de chaque configuration
-    for i in range(N + 1):
-        for j in range(N + 1):
-            for k in range(-Mp, Mp + 1):
-                table[i, j, k] = Configuration(i, j, k, M)
-                # print("La Configuration x={}, y={}, t={} a été initialisée.".format(i, j, k))
-                # print(table[i, j, k].__str__())
+    N = 50
+    m = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    for M in m:
+        # M = 7
+        Mp = M // 2
+        table = dict()
+        # Les cas triviaux sont initialisés lors de la creation de chaque configuration
+        for i in range(N + 1):
+            for j in range(N + 1):
+                for k in range(-Mp, Mp + 1):
+                    table[i, j, k] = Configuration(i, j, k, M)
 
-    # Puis on calcule le g_opt et s_opt de toutes les configurations en allant des plus petites aux plus grandes.
-    for x in range(2, N + 1):
+        # Puis on calcule le g_opt et s_opt de toutes les configurations en allant des plus petites aux plus grandes.
         for y in range(2, N + 1):
-            for t in range(-Mp + 1, Mp):
-                # print("Calcul de g_opt et s_opt pour i={}, j={}, k={}".format(i, j, k))
-                # if table[i, j, k] == "n/c":
-                table[x, y, t].calc_gs_opt(table)
+            for x in range(2, N + 1):
+                for t in range(-Mp + 1, Mp):
+                    table[x, y, t].calc_gs_opt(table)
 
-    # g_opt = table[N, N, 0].g_opt
-    # s_opt = table[N, N, 0].s_opt
-    # print(g_opt, s_opt)
-    print(table[N, N, 0].__str__())
+        print(table[N, N, 0].__str__())
+        export_fichier(N, M, table)
 
 
 if __name__ == "__main__":
